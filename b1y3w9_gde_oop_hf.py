f35dd47106e3300d0999439d19098124f1efc021
b1y3w9 <b1y3w9@neptun.gde.hu>
2024-04-20 16:32:40 +0200

    Foglalások kezelése, felhasználó interfész és adatvalidáció

--- b1y3w9_gde_oop_hf.py
+++ b1y3w9_gde_oop_hf.py
@@ -52,3 +52,76 @@ class Foglalas:
 
     def __str__(self):
         return f"Szoba: {self.szoba.szobaszam}, Dátum: {self.datum}"
+
+def main():
+    # Szálloda létrehozása
+    szalloda = Szalloda("Példa Szálloda")
+
+    # Szobák létrehozása és hozzáadása a szállodához
+    egyagyas_szoba1 = EgyagyasSzoba("101", 5000)
+    ketagyas_szoba1 = KetagyasSzoba("202", 8000)
+    ketagyas_szoba2 = KetagyasSzoba("203", 8100)
+
+    szalloda.szoba_hozzaad(egyagyas_szoba1)
+    szalloda.szoba_hozzaad(ketagyas_szoba1)
+    szalloda.szoba_hozzaad(ketagyas_szoba2)
+
+    # Foglalások létrehozása és hozzáadása a szállodához
+    foglalas1 = Foglalas(egyagyas_szoba1, datetime(2024, 4, 1))
+    foglalas2 = Foglalas(ketagyas_szoba1, datetime(2024, 4, 3))
+    foglalas3 = Foglalas(ketagyas_szoba1, datetime(2024, 5, 3))
+
+    szalloda.foglalasok = [foglalas1, foglalas2, foglalas3]
+
+
+# Felhasználói interfész
+    while True:
+        print("\n1. Foglalás")
+        print("2. Lemondás")
+        print("3. Foglalások listázása")
+        print("4. Kilépés")
+
+        valasztas = input("Válasszon egy műveletet: ")
+
+        if valasztas == "1":
+            szobaszam = input("Adja meg a szobaszámot: ")
+            datum_str = input("Adja meg a foglalás dátumát (YYYY-MM-DD): ")
+            datum = datetime.strptime(datum_str, "%Y-%m-%d")
+            # Szoba ellenőrzése
+            for szoba in szalloda.szobak:
+                if szoba.szobaszam == szobaszam:
+                    # Dátum ellenőrzése
+                    if datum < datetime.now():
+                        print("Érvénytelen dátum! Kérem adjon meg jövőbeli dátumot.")
+                        break
+                    foglalas = Foglalas(szoba, datum)
+                    szalloda.foglalas_felvetel(foglalas)
+                    print("Foglalás sikeresen rögzítve.")
+                    break
+            else:
+                print("Nincs ilyen szoba.")
+
+        elif valasztas == "2":
+            szobaszam = input("Adja meg a szobaszámot: ")
+            datum_str = input("Adja meg a foglalás dátumát (YYYY-MM-DD): ")
+            datum = datetime.strptime(datum_str, "%Y-%m-%d")
+            for foglalas in szalloda.foglalasok:
+                if foglalas.szoba.szobaszam == szobaszam and foglalas.datum == datum:
+                    szalloda.foglalas_lemondas(foglalas)
+                    break
+            else:
+                print("Nincs ilyen foglalás.")
+
+        elif valasztas == "3":
+            szalloda.foglalasok_listazasa()
+
+        elif valasztas == "4":
+            print("Kilépés...")
+            break
+
+        else:
+            print("Érvénytelen választás!")
+
+
+if __name__ == "__main__":
+    main()
