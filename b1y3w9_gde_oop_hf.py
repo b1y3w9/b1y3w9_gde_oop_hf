72203c0055ae6e6544b121e22cbd14d4f8af7dad
b1y3w9 <b1y3w9@neptun.gde.hu>
2024-04-20 15:25:13 +0200

    Foglalás class létrehozása

--- b1y3w9_gde_oop_hf.py
+++ b1y3w9_gde_oop_hf.py
@@ -44,3 +44,11 @@ class Szalloda:
         print("Foglalások:")
         for foglalas in self.foglalasok:
             print(foglalas)
+
+class Foglalas:
+    def __init__(self, szoba, datum):
+        self.szoba = szoba
+        self.datum = datum
+
+    def __str__(self):
+        return f"Szoba: {self.szoba.szobaszam}, Dátum: {self.datum}"
